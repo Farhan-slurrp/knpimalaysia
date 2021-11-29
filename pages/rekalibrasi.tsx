57c8478a1ffa3e52c5eb417392c317fa9026a543
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-11-30 02:23:21 +0800

    embed google form

---
+++ pages/rekalibrasi.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface Props {}
+
+const Rekalibrasi = (props: Props) => {
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeZLsXUWt4pzfOVfgfkI2RZjFoOkQ9rX-IQEHAvF1merdNkbA/viewform?embedded=true"
+        className="w-full"
+        height="3471"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+export default Rekalibrasi;
