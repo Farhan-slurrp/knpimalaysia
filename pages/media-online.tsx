268e678706ead552fd1ebfcd2f23ddb57b95453d
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-11-30 01:11:14 +0800

    small change

--- pages/media-online.tsx
+++ pages/media-online.tsx
@@ -5,7 +5,7 @@ interface Props {}
 const MediaOnline = (props: Props) => {
   return (
     <div className="p-4">
-      <p className="py-6">KNPI Garap Pemuda Indonesia di Luar Negeri</p>
+      <p className="py-6 text-xl">KNPI Garap Pemuda Indonesia di Luar Negeri</p>
       <iframe
         className="w-full h-80"
         src="https://www.jpnn.com/news/knpi-garap-pemuda-indonesia-di-luar-negeri"
