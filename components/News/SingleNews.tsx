1df7eaa28332c4abdccb0779031891ded1796a31
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-12-03 03:51:28 +0800

    add back button for news

--- components/News/SingleNews.tsx
+++ components/News/SingleNews.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { INews } from "../../utils/news";
+import { useRouter } from "next/router";
 
 interface Props {
   news: INews | undefined;
 }
 
 const SingleNews = ({ news }: Props) => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col gap-10 px-4 py-12">
       <p className="text-2xl font-semibold text-center text-gray-900">
@@ -19,6 +22,14 @@ const SingleNews = ({ news }: Props) => {
         ></iframe>
       )}
       {news && news["content"] && <p>{news["content"]}</p>}
+      <div className="flex justify-center">
+        <button
+          onClick={() => router.back()}
+          className="px-4 py-2 text-blue-500 border border-blue-500 rounded-md hover:text-white hover:bg-blue-500"
+        >
+          Kembali
+        </button>
+      </div>
     </div>
   );
 };
