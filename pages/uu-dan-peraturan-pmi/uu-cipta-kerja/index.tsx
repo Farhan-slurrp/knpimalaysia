e6d77da28fa28c4c73a572c89cbe7dc65cae0d8a
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-12-05 05:40:15 +0800

    minor changes

--- pages/uu-dan-peraturan-pmi/uu-cipta-kerja/index.tsx
+++ pages/uu-dan-peraturan-pmi/uu-cipta-kerja/index.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { useRouter } from "next/router";
 
 interface Props {}
 
 const UUCiptaKerja = (props: Props) => {
+  const router = useRouter();
   return (
     <div>
       <img src="/under-construction.jpeg" alt="img" className="w-full p-8" />
+      <div className="flex justify-center pt-8">
+        <button
+          onClick={() => router.back()}
+          className="px-4 py-2 text-white bg-blue-500 rounded-md"
+        >
+          Kembali
+        </button>
+      </div>
     </div>
   );
 };
