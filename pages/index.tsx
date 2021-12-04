03dce8377b8bdb5988683cbfeb0a73fb131ac742
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-12-05 02:27:40 +0800

    add page under construction image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <main>
-        <p>test</p>
+      <main className="p-8">
+        <img
+          src="/under-construction.jpeg"
+          alt="under-construction"
+          className="w-full"
+        />
       </main>
       <Footer />
     </div>
