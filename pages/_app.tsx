89fc96c9fdf094be21de7ab91a3b63070c307265
Farhan-slurrp <farhanlmntrix@gmail.com>
2022-01-03 23:04:12 +0800

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
 import "tailwindcss/tailwind.css";
 import { AppStoreProvider } from "../stores/AppContext";
 import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
