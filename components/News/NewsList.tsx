39b4fc3645a2897c798b83cd7eb84f8052021910
Farhan Ahmad Nurzi <58872254+Farhan-slurrp@users.noreply.github.com>
2022-01-19 23:37:14 +0800

    done some more stuff

--- components/News/NewsList.tsx
+++ components/News/NewsList.tsx
@@ -12,41 +12,43 @@ interface Props {
 
 const NewsList = ({ title, allNews, baseLink }: Props) => {
   const router = useRouter();
+
   return (
     <div className="w-screen md:w-full p-2 md:p-4 flex flex-col items-center">
       <h2 className="py-4 text-center md:text-left text-lg md:text-2xl mb-8 font-semibold text-gray-900">
         {title}
       </h2>
       <div className="flex flex-col gap-4 items-center md:items-stretch min-w-2">
-        {allNews.map((news) => (
-          <div
-            key={news["id"]}
-            className="flex items-center gap-4 p-4 border border-gray-300 rounded-md cursor-pointer"
-            onClick={() => router.push(`/${baseLink}/${news["id"]}`)}
-          >
-            <img
-              src={news["thumbnail"] ? news["thumbnail"] : "/knpi.png"}
-              alt="no-img"
-              width="80"
-              height="70"
-              className="border border-gray-400"
-            />
-            <div className="flex flex-col justify-center">
-              <p className="text-xl hover:text-blue-500 hover:underline text-ellipsis">
-                {news["title"]}
-              </p>
-              <p className="text-xs text-gray-800">
-                Tanggal Terbit:{" "}
-                {format(new Date(news["publishDate"]!), "dd MMMM yyyy")}
-              </p>
-              {news["type"] == "external" && (
-                <p className="text-xs text-gray-800">
-                  Sumber: {news["publisher"]}
+        {allNews.length > 0 &&
+          allNews.map((news) => (
+            <div
+              key={news["id"]}
+              className="flex items-center gap-4 p-4 border border-gray-300 rounded-md cursor-pointer"
+              onClick={() => router.push(`/${baseLink}/${news["id"]}`)}
+            >
+              <img
+                src={news["thumbnail"] ? news["thumbnail"] : "/knpi.png"}
+                alt="no-img"
+                width="80"
+                height="70"
+                className="border border-gray-400"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-xl hover:text-blue-500 hover:underline text-ellipsis">
+                  {news["title"]}
                 </p>
-              )}
+                <p className="text-xs text-gray-800">
+                  Tanggal Terbit:{" "}
+                  {format(new Date(news["publishDate"]!), "dd MMMM yyyy")}
+                </p>
+                {news["type"] == "external" && (
+                  <p className="text-xs text-gray-800">
+                    Sumber: {news["publisher"]}
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
