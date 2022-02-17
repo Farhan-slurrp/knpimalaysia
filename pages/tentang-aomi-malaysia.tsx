5370f54c281f301fd44ec7a4beed951312b35ac4
Farhan Ahmad Nurzi <58872254+Farhan-slurrp@users.noreply.github.com>
2022-02-18 03:03:03 +0800

    edit tentang aomi page

--- pages/tentang-aomi-malaysia.tsx
+++ pages/tentang-aomi-malaysia.tsx
@@ -10,7 +10,104 @@ const TentangAOMI = (props: Props) => {
       <a href="https://aomi.org.my" target="_blank">
         <img src="/aomi.png" alt="aomi" className="w-full" />
       </a>
-      <p className="text-lg font-semibold text-center">
+      <div className="text-justify">
+        <p>
+          Aliansi Organisasi Masyarakat Indonesia di Malaysia (AOMI Malaysia)
+          adalah wadah bersama dari berbagai lapisan masyarakat dalam rangka
+          mewujudkan silaturrahim, komunikasi, koordinasi, kerjasama, dan
+          sharing center dengan semangat kebangsaan dan kemanusiaan berasaskan
+          manfaat dan kebaikan bersama, terutama bagi Warga Negara Indonesia
+          (WNI) dan Pekerja Migran Indonesia (PMI).
+        </p>
+        <p>
+          Seperti yang telah diketahui, bahwa di Malaysia terdapat banyak
+          assosiasi, organisasi, komunitas, dan paguyuban masyarakat Indonesia
+          dengan latar belakang dan konsentrasi bidang yang berbeda-beda. Peran,
+          rekam jejak, kepeduliannya dalam bentuk program-program kebaikan telah
+          terbukti dan dirasakan secara langsung oleh WNI dan PMI di Malaysia.
+          Sehingga potensi ini perlu untuk kemudian dikelola, disinergikan dan
+          ditingkatkan bersama-sama.
+        </p>
+        <p>
+          Atas dasar keinginan luhur, dengan penuh kesadaran, dan ikhtiar yang
+          terbaik untuk menjadi bagian solusi atas problematika/tantangan
+          kebangsaan dan kemanusian, khususnya yang berhubungan dengan WNI dan
+          PMI di Malaysia, maka lahirlah inisiatif untuk bersepakat bersama
+          membentuk wadah yang bernama Aliansi Organisasi Masyarakat Indonesia
+          (AOMI) di Malaysia.
+        </p>
+      </div>
+      <div className="text-justify">
+        <h2 className="md:text-lg font-semibold">Visi AOMI</h2>
+        <p>
+          Menjadi wadah sinergi organisasi masyarakat Indonesia di Malaysia yang
+          terdepan, inklusif dan independen dengan mengedepankan spirit
+          kebangsaan dan kemanusiaan dengan menjadi mitra strategis dan
+          konstruktif bagi KBRI di Malaysia di berbagai isu penting terkait
+          Pekerja Migran Indonesia. Menjadi mitra strategis bagi Organisasi
+          Masyarakat Malaysia, maupun lembaga-lembaga resmi dari Kerajaan
+          Malaysia dalam konteks pengurusan Pekerja Migran Indonesia.
+        </p>
+      </div>
+      <div className="text-justify">
+        <h2 className="md:text-lg font-semibold">Misi AOMI</h2>
+        <p>
+          Mempererat hubungan Kebangsaan dan menyamakan langkah dalam merespon
+          isu Pekerja Migran Indonesia (PMI)/WNI di Malaysia pada khususnya dan
+          Indonesia pada umumnya Membantu menjaga dan meningkatkan kualitas
+          relasi antara KBRI dengan Kerajaan Malaysia, sehingga tercipta
+          hubungan diplomatik yang semakin baik dan menguat antara kedua negara,
+          khususnya dalam Pekerja Migran Indonesia.
+        </p>
+        <p>
+          Membantu meningkatkan kualitas relasi antara KBRI dengan Pekerja
+          Migran Indonesia, dalam berbagai isu penting sehingga semakin banyak
+          solusi yang direaliasikan serta semakin banyak problematika yang bisa
+          diselesaikan. Memberikan dampak positif terhadap penguatan hubungan
+          bilateral antara Indonesia dengan Malaysia melalui berbagai program
+          sosial yang berhubungan dengan pengurusan PMI di Malaysia.
+          Memberdayakan sinergi potensi dari berbagai ormas dalam rangka
+          merespon isu sosial Pekerja Migran Indonesia di Malaysia.
+        </p>
+      </div>
+
+      <div className="text-justify">
+        <h2 className=" mb-2 font-semibold">
+          AOMI Malaysia memiliki tujuan sebagai berikut :
+        </h2>
+        <p>
+          Terwujudnya persatuan dan kesatuan seluruh komponen masyarakat
+          Indonesia di Malaysia, demi kemaslahatan seluruh WNI di Malaysia pada
+          khususnya, dan juga WNI di tanah air ataupun di mancanegara pada
+          umumnya.
+        </p>
+        <p>
+          Terberdayakannya seluruh potensi Masyarakat Indonesia di Malaysia
+          dalam berbagai dimensi kehidupan berbangsa dan bernegara demi
+          terwujudnya masyarakat Indonesia yang adil, makmur dan sejahtera
+          berdasarkan Pancasila dan UUD 1945
+        </p>
+      </div>
+
+      <div className="text-justify">
+        <h2 className=" mb-2 font-semibold">
+          AOMI memiliki fungsi, sebagai berikut :
+        </h2>
+        <p>
+          Sebagai wadah perekat kemajemukan masyarakat Indonesia di Malaysia
+          dalam rangka meningkatkan kualitas kehidupan kemasyarakatan, berbangsa
+          dan bernegara guna mempercepat usaha pencapaian tujuan nasional
+          Sebagai laboratorium masyarakat Indonesia di Malaysia dalam rangka
+          mengembangkan potensi masyarakat yang berwawasan kebangsaan, mandiri
+          dan bertanggungjawab.
+        </p>
+        <p>
+          Sebagai wadah perjuangan masyarakat Indonesia dalam rangka peningkatan
+          derajat, taraf hidup, status dan kesejahteraan sosial, guna
+          mempercepat terciptanya masyarakat yang adil, makmur dan sejahtera.
+        </p>
+      </div>
+      <p className="pt-4 text-lg font-semibold text-center">
         “Bekerja-sama dan Sama-sama Bekerja | Memupuk Kebersamaan Dalam
         Keberagaman”
       </p>
