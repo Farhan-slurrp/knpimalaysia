a8d3e0138d71910c3f55044f93f76948ef898d2f
ikhwa-beep <ikhwanul.effendie@gmail.com>
2025-06-10 00:00:52 +0800

    embed correct gdrive link and set proper iframe size and add title

--- pages/galeri-foto.tsx
+++ pages/galeri-foto.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 const MyIframe: React.FC = () => {
   return (
+    <div className="px-2 md:px-24">
+      <h1 className="pt-16 pb-8 md:text-xl font-semibold text-center">
+        GALERI FOTO BP KNPI MALAYSIA
+      </h1>
+    
     <iframe
-      src="https://drive.google.com/embeddedfolderview?id=1L3jwLc7UHJ8KBPjO8_yp8kzPDxzL5VjW#grid"
-      width="600"
-      height="600"
+      src="https://drive.google.com/embeddedfolderview?id=1bb9d91Lw7ceW8rD1OwDwzzdZHEOfYzki#grid"
+      width="1080"
+      height="1080"
       title="Gdrive embed"
       allow="accelerometer; encrypted-media; gyroscope"
       referrerPolicy="strict-origin-when-cross-origin"
     />
+
+    </div>
   );
 };
 
