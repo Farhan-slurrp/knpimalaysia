af82866cd23ba574b4d0403fafd9067cb9ea0eb4
ikhwa-beep <ikhwanul.effendie@gmail.com>
2025-06-09 22:29:04 +0800

    Delete Nomor WhatsApp

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,29 +7,15 @@ const Footer = (props: Props) => {
   return (
     <div className="flex flex-col items-center p-4 text-xs text-gray-800 border-t border-gray-300">
       <p className="text-center">
-        Alamat Sekretariat: Lot 108, Jalan Ampang,{" "}
+        Alamat Sekretariat: 101, Jalan Putra,{" "}
         <br className="md:hidden block" />
-        Desa Pahlawan, 55000, Kuala Lumpur, Malaysia
+        Chow Kit, 50350, Kuala Lumpur, Malaysia.
       </p>
       <div className="flex items-center gap-6">
         <p className="flex items-center">
-          <img
-            src="http://assets.stickpng.com/images/584856b4e0bb315b0f7675ac.png"
-            alt="email"
-            width="28"
-            height="17"
-          />{" "}
-          knpi.malaysia@gmail.com
+          knpi.malaysia@gmail.com {" "}
         </p>
-        <p className="flex items-center">
-          <img
-            src="https://w7.pngwing.com/pngs/672/164/png-transparent-whatsapp-icon-whatsapp-logo-computer-icons-zubees-halal-foods-whatsapp-text-circle-unified-payments-interface.png"
-            alt="email"
-            width="20"
-            height="20"
-          />{" "}
-          +60 12-6269 565
-        </p>
+
       </div>
     </div>
   );
